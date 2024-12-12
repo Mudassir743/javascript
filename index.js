e22d9fb5d0129534103edb24df22b1f8d2dd4045
Mudassir743 <mudassirahmed9125@gmail.com>
2024-12-12 13:39:00 +0500

    string properties

--- index.js
+++ index.js
@@ -190,6 +190,17 @@
 //   }
 // }
 //showProperties(movie);
+
+//  ≈≈≈≈≈≈≈≈≈≈≈≈sum of multiples ≈≈≈≈≈≈≈≈≈≈≈≈
+
+function sum(limit) {
+  let sum = 0;
+  for (let i = 0; i < limit; i++) {
+    if (i % 3 == 0 || i % 5 == 0);
+  }
+}
+console.log(sum(10));
+
 ///////TO REPEAT///// uper side .........
 
 //persentage /; /////// WALA/
